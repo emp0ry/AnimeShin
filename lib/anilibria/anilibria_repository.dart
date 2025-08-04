0c201d7a91a1fc9226cae146d59fc27e8c0a04e2
emp0ry <avagyanerik13@gmail.com>
2025-08-04 20:00:16 +0400

    Fix Limitation

--- lib/anilibria/anilibria_repository.dart
+++ lib/anilibria/anilibria_repository.dart
@@ -7,33 +7,67 @@ class AnilibriaRepository {
   Future<Map<String, dynamic>> fetchListByAliases({
     required List<String> aliases,
     int page = 1,
-    int limit = 10,
+    int limit = 50,
     List<String>? include,
     List<String>? exclude,
   }) async {
-    final params = <String, String>{
-      'aliases': aliases.join(','),
-      'page': '$page',
-      'limit': '$limit',
-    };
+    if (aliases.length <= limit) {
+      final params = <String, String>{
+        'aliases': aliases.join(','),
+        'page': '$page',
+        'limit': '$limit',
+      };
 
-    if (include != null && include.isNotEmpty) {
-      params['include'] = include.join(',');
+      if (include != null && include.isNotEmpty) {
+        params['include'] = include.join(',');
+      }
+      if (exclude != null && exclude.isNotEmpty) {
+        params['exclude'] = exclude.join(',');
+      }
+
+      final uri = Uri.parse(_baseUrl).replace(queryParameters: params);
+
+      final response = await http.get(uri);
+      if (response.statusCode != 200) {
+        throw Exception('Failed to load Anilibria list');
+      }
+
+      return json.decode(response.body) as Map<String, dynamic>;
     }
-    if (exclude != null && exclude.isNotEmpty) {
-      params['exclude'] = exclude.join(',');
+
+    final Map<String, dynamic> allResult = {'data': <dynamic>[]};
+
+    for (var i = 0; i < aliases.length; i += limit) {
+      final batch = aliases.sublist(
+        i,
+        i + limit > aliases.length ? aliases.length : i + limit,
+      );
+      final params = <String, String>{
+        'aliases': batch.join(','),
+        'page': '$page',
+        'limit': '$limit',
+      };
+
+      if (include != null && include.isNotEmpty) {
+        params['include'] = include.join(',');
+      }
+      if (exclude != null && exclude.isNotEmpty) {
+        params['exclude'] = exclude.join(',');
+      }
+
+      final uri = Uri.parse(_baseUrl).replace(queryParameters: params);
+
+      final response = await http.get(uri);
+      if (response.statusCode != 200) {
+        throw Exception('Failed to load Anilibria list');
+      }
+
+      final batchResult = json.decode(response.body) as Map<String, dynamic>;
+      if (batchResult['data'] is List) {
+        allResult['data'].addAll(batchResult['data']);
+      }
     }
 
-    final uri = Uri.parse(_baseUrl).replace(queryParameters: params);
-
-    // print('DEBUG: uri for Anilibria: $uri');
-
-    final response = await http.get(uri);
-
-    if (response.statusCode != 200) {
-      throw Exception('Failed to load Anilibria list');
-    }
-
-    return json.decode(response.body) as Map<String, dynamic>;
+    return allResult;
   }
 }
